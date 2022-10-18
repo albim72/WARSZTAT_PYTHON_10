5a927c55d4e8ba0edef28ad85f9382819f2280cf
albim72 <marcin.albiniak72@gmail.com>
2022-10-18 11:05:03 +0200

    Create podstawy.py

---
+++ D1/podstawy.py
@@ -0,0 +1,13 @@
+print("to linia na dobry początek")
+
+#komentarz - kolekcja: lista
+
+"""
+komentarz dokumentacyjny wieloliniowy
+"""
+
+'''
+komentarz wieloliniowy
+'''
+
+imiona = ["Jan","Paweł","Anna","Olga","Leon","Tekla","Grzegorz","Magda"]
