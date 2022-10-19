fbb872535f4fb5572e392c00eca13d081cdb6216
albim72 <marcin.albiniak72@gmail.com>
2022-10-19 13:42:16 +0200

    Update main.py

--- D2/MODULY/main.py
+++ D2/MODULY/main.py
@@ -1,1 +1,12 @@
+# import dane
+# import dane as dn
+from dane import book as ksiazka
+from dane import nb as liczba
+from bfunkcje import czytaj,czytaj_dict
+
+print(ksiazka)
+print(liczba)
+
+czytaj(liczba)
+czytaj_dict(ksiazka)
 
