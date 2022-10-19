da51cb32c71fcf8028fe87df015e717b62fdfbea
albim72 <marcin.albiniak72@gmail.com>
2022-10-19 13:58:04 +0200

    Update main.py

--- D2/MODULY/main.py
+++ D2/MODULY/main.py
@@ -3,10 +3,16 @@
 from dane import book as ksiazka
 from dane import nb as liczba
 from bfunkcje import czytaj,czytaj_dict
+from cls.sdane.czytaniedanych import CDane
 
 print(ksiazka)
 print(liczba)
 
 czytaj(liczba)
 czytaj_dict(ksiazka)
+print("________________z klasy________________")
+cd = CDane(liczba,ksiazka)
+
+cd.czytaj_l()
+cd.czytaj_d()
 
