7d40bfb68f073e05fb309d325b7f543006315643
albim72 <marcin.albiniak72@gmail.com>
2022-10-19 11:18:58 +0200

    Update generatory.py

--- D2/generatory.py
+++ D2/generatory.py
@@ -30,3 +30,37 @@ for i in wznowienie(3,4):
     if i == 1001:
         continue
     print(f"zwrócono wartość: {i}")
+
+#generator z return
+
+def genret():
+    for i in range(5):
+        if i==3:
+            print("przerwanie")
+            return
+        else:
+            yield i
+
+for x in genret():
+    print(x)
+
+#generator złozony
+def complexgen():
+    x = 0
+    while True:
+        y = yield x
+        if y is None:
+            x = x+1
+        else:
+            x=y
+
+
+g = complexgen()
+
+print(next(g))
+print(next(g))
+print(next(g))
+print(g.send(121))
+print(next(g))
+print(next(g))
+print(next(g))
