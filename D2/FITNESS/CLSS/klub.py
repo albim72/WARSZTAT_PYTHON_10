f6e4b85bbbac9d1e201ac48ab072aff7ad616f96
albim72 <marcin.albiniak72@gmail.com>
2022-10-19 15:02:36 +0200

    Update klub.py

--- D2/FITNESS/CLSS/klub.py
+++ D2/FITNESS/CLSS/klub.py
@@ -1,9 +1,13 @@
 class Klub:
-    
+
     def __init__(self,nr,nazwa,adres):
         self.nr = nr
         self.nazwa = nazwa
         self.adres = adres
-        
+        self.przypisanie()
+
     def infoklub(self):
         return f"klub {self.nazwa}, nr filii: {self.nr}"
+
+    def przypisanie(self):
+        print(f"przypisanie do klubu nr :{self.nr}")
