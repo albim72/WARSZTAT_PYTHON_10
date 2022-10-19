06f5ec3b936d1afe4f9d7c0fab83d529b7d2c9d5
albim72 <marcin.albiniak72@gmail.com>
2022-10-19 15:55:56 +0200

    Update main.py

--- D2/FITNESS/main.py
+++ D2/FITNESS/main.py
@@ -1,6 +1,6 @@
 from clss.osoba import Osoba
 from clss.trener import Trener
-from clss.klient import
+from clss.klient import Klient
 
 os1 = Osoba("Feliks",43,88,175)
 os1.print_osoba()
@@ -20,3 +20,27 @@ tr1.print_osoba()
 print(f"czy osoba jest trenerem personalnym? ({tr1.czytrener()})")
 print(tr1.infoklub())
 tr1.print_trener()
+
+print("___________________________________________________________")
+
+kl1 = Klient("Leon",27,80,180,678,"Lublin",nr_kb=26)
+kl1.print_osoba()
+print(f"czy osoba jest trenerem personalnym? ({kl1.czytrener()})")
+
+print("___________________________________________________________")
+
+kl2 = Klient("Anna",26,55,171,7654,"Kraków",7775,8,True,5,"Euforia","Wesoła 6, Wąchock")
+kl2.print_osoba()
+print(f"czy osoba jest trenerem personalnym? ({kl2.czytrener()})")
+kl2.print_trener()
+kl2.infoklub()
+
+
+print("___________________________________________________________")
+
+kl3 = Klient("Edyta",22,78,177,678567,"Zamość",nr_kb=26,dyscyplina="biegi ultra",lata_upr=3,
+             zyciowka="105km 20h 16min 4s")
+kl3.print_osoba()
+print(f"czy osoba jest trenerem personalnym? ({kl3.czytrener()})")
+print(kl3.infosport())
+
