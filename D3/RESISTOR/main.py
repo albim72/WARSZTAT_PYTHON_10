2efa7d59bc8afa45d1a049f40d209cdb411b44e2
albim72 <marcin.albiniak72@gmail.com>
2022-10-20 10:44:35 +0200

    Update main.py

--- D3/RESISTOR/main.py
+++ D3/RESISTOR/main.py
@@ -1,5 +1,9 @@
 from oldresistor import OldResistor
+from resistor import Resistor
+from voltage import VoltageResistance
 
+print("______________________________________")
+print("klasa OldResistor")
 r0 = OldResistor(10.2E2)
 print(r0)
 print(r0._ohms)
@@ -9,3 +13,22 @@ print(r0._ohms)
 r0.set_ohms(3.33E2)
 
 print(r0.get_ohms())
+print("______________________________________")
+print("klasa Resistor")
+r1 = Resistor(50E3)
+r1.ohms = 10E3
+
+print(f'układ elektryczny:\noporność: {r1.ohms} om\nnapięcie prądu: {r1.voltage} V\n'
+      f'natężenie prądu: {r1.current} A')
+
+print("______________________________________")
+print("klasa VoltageResistance")
+
+r2 = VoltageResistance(1E3)
+print(f'natężenie początkowe prądu: {r2.current} A')
+r2.voltage = 25
+print(f'natężenie prądu: {r2.current} A')
+
+print(f'napięcie: {r2.voltage} V')
+
+
