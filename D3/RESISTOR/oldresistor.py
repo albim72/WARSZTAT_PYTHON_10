a1c858a3e425829b1c8c4c47979c28be1a89eec1
albim72 <marcin.albiniak72@gmail.com>
2022-10-20 10:01:38 +0200

    Update oldresistor.py

--- D3/RESISTOR/oldresistor.py
+++ D3/RESISTOR/oldresistor.py
@@ -3,4 +3,8 @@ class OldResistor:
     def __init__(self,ohms):
         self._ohms = ohms
 
+    def get_ohms(self):
+        return self._ohms
 
+    def set_ohms(self,ohms):
+        self._ohms = ohms
