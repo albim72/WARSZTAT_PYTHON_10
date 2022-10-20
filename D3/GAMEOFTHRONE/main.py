c05f4fdf67a3fab04b08ffbea3c5f6f40c4070e7
albim72 <marcin.albiniak72@gmail.com>
2022-10-20 14:52:18 +0200

    Update main.py

--- D3/GAMEOFTHRONE/main.py
+++ D3/GAMEOFTHRONE/main.py
@@ -1,8 +1,10 @@
 from tywin import Tywin
 from tyrion import Tyrion
+from cersei import Cersei
 
 tyw = Tywin("Lannister","Lord","m",3,8,9)
 tyr = Tyrion("Lannister","Lord","m",5,10,8)
+cer = Cersei("Lannister","Queen","k",3,10,10)
 
 print("_________ Lord Tywin ______________")
 tyw.walka()
@@ -13,3 +15,9 @@ print("_________ Lord Tyrion ______________")
 tyr.walka()
 tyr.negocjacja()
 tyr.uczta()
+
+print("_________ Queen Cersei ______________")
+cer.walka()
+cer.negocjacja()
+cer.uczta()
+
