3d121bd4b5ba0a1bad8f10ad47f1fbbe59e78cd9
albim72 <marcin.albiniak72@gmail.com>
2022-10-21 09:53:21 +0200

    Update interfejs.py

--- D4/interfejs.py
+++ D4/interfejs.py
@@ -26,4 +26,22 @@ class IPojazd:
     def koszt_przejazdu(self,odl,litry,cena_l):
         raise NotImplementedError
 
+class Pojazd(IPojazd):
 
+    def silnik(self, rodzaj_poj, kategoria, marka, model, rodzaj_silnika, poj):
+        return f"Pojazd -> {rodzaj_poj}, {kategoria}, marka:{marka}, model: {model}," \
+               f" rodzaj silnika: {rodzaj_silnika}, pojemność:; {poj}."
+
+    def trasa(self, start, koniec, odl, czas_przejazdu):
+        trasa = f"początek trasy: {start}, koniec trasy: {koniec}"
+        przej = f"odległość: {odl} km, czas przejazdu {czas_przejazdu} h"
+        return trasa,przej
+
+    def set_paliwo(self, cena_l):
+        super().set_paliwo(cena_l)
+
+    def spal_100(self, odl, litry):
+        return litry*100/odl
+
+    def koszt_przejazdu(self, odl, litry, cena_l):
+        return self.spal_100(odl,litry)*(odl/100)*cena_l
