6b669669e5a6d206edc9462844ec25ad19c9db8f
albim72 <marcin.albiniak72@gmail.com>
2022-10-21 14:57:19 +0200

    Update dodaj_pracownika.py

--- D4/dodaj_pracownika.py
+++ D4/dodaj_pracownika.py
@@ -1,1 +1,19 @@
+import json
 
+def dodaj_nowego_pracownika(new_data,klucz,filename = "pracownik.json"):
+    with open(filename,"r+",encoding="utf-8") as file:
+        file_data = json.load(file)
+        file_data[klucz].append(new_data)
+        file.seek(0)
+        json.dump(file_data,file,indent=4)
+
+
+nowyprac = {
+            "imie": "Jarosław",
+            "nazwisko": "Kowal",
+            "stanowisko": "informatyk",
+            "lata_pracy": 8,
+            "email": "jarek88@firma.pl"
+        }
+
+dodaj_nowego_pracownika(nowyprac,"pra_info")
